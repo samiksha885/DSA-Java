b66f643827bb4c3819231f4e7d5db2da9a053452
samiksha pandey <samikshapandey25122005@gmail.com>
2026-06-06 21:12:03 +0530

    largestNumber
    Add the implementation for the largest number problem.

---
+++ Arrays/largestNumber.java
@@ -0,0 +1,18 @@
+//largest number in aaryy
+public class largestNumber {
+    public static int getLargest(int numbers[]){
+        int largest = Integer.MIN_VALUE;//infinty
+        
+        for(int i=0; i<numbers.length; i++){
+            if(largest<numbers[i]){
+                largest = numbers[i];
+            }
+        }
+         return largest;
+    }
+
+    public static void main(String args[]){
+        int numbers[] = {1,2,6,3,5};
+        System.out.println("largest value is : " + getLargest(numbers));
+    }
+}
