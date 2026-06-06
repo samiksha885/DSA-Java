4f79d4aacf99144791bad7d3fad572c25c7a5f3e
samiksha pandey <samikshapandey25122005@gmail.com>
2026-06-06 20:40:30 +0530

    linearSearch
    Add linearSearch Solution.

---
+++ Arrays/linearSearch.java
@@ -0,0 +1,21 @@
+public class linearSearch{
+   public static int linearsearch(int num[],int key){
+       for(int  i = 0; i<num.length;i++){
+         if(num[i] == key){
+            return i;
+         }
+       }
+        return -1; 
+   }
+
+   public static void main(String[] args){
+      int num[] = {2,4,6,7,8,9,1,2,3,6,8};
+      int key = 6;
+      int index = linearsearch(num, key);
+      if(index == -1){
+         System.out.println("Element not found");
+      } else{
+         System.out.println("Element found at index: " + index);
+      }
+   }
+}
