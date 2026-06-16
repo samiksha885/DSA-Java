914d7c93c9e653dce99d42d46ce5f67b25c09628
samiksha pandey <samikshapandey25122005@gmail.com>
2026-06-16 22:47:47 +0530

    buyAndsell.java
    Add solution for the Buy and Sell Stock problem.
    
    Implemented the optimal approach to solve the "Best Time to Buy and Sell Stock" problem using a single-pass algorithm. The code tracks the minimum price encountered so far and calculates the maximum potential profit at each step, ensuring an efficient O(n) time complexity.

---
+++ Arrays/buyandsell.java
@@ -0,0 +1,28 @@
+//You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+//You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+//Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+  //tc = o(n);
+public class buyandsell{
+    public static int buyAndsellstocks(int prices[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i = 0; i<prices.length; i++){
+            if(buyPrice < prices[i]){ //prices[i ] = sellingprice
+                  int profit = prices[i] - buyPrice;
+                  maxProfit = Math.max(maxProfit , profit);
+            }else{
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
+
+    public static void main(String args[]){
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(buyAndsellstocks(prices));
+    }
+}
